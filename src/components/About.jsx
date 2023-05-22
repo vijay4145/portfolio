96fd39b9f875bf4d0baeba3c3fb0ec0b6ce65cbc
vijay4145 <svijay4145@gmail.com>
2023-05-22 22:48:56 +0530

    minor update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ export const About = ({setComponentVisible}) => {
     <div ref={ref}>
       <Element name='about' className='p-5 text-lg flex flex-col flex-wrap items-center text-center'>
         <h5 className='text-2xl'>𝐀𝐛𝐨𝐮𝐭 :</h5>
-        <p className='max-sm:text-base'>Hello, I am third year Computer Engineering student at Terna Engineering College and I'm passionate in developing Web apps. Along with this I'm also a Competitive programmer with some knowledge of Android Development. I love to code and I consider Software Development to be an incredibly creative discipline that makes difference in people's lives. I enjoy creating application which will be able to impact many user.</p>
+        <p className='max-sm:text-base'>Hello, I am final year Computer Engineering student at Terna Engineering College and I'm passionate in developing Web apps. Along with this I'm also a Competitive programmer with some knowledge of Android Development. I love to code and I consider Software Development to be an incredibly creative discipline that makes difference in people's lives. I enjoy creating application which will be able to impact many user.</p>
 
       </Element>
     </div>
